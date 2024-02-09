f06019553a816db18febcdb7143814c988a77fec
marco-tav <marco.tavella@outlook.com>
2024-02-09 18:15:44 -0300

    Add code to make button salmon color when clicked and run production build

--- src/index.js
+++ src/index.js
@@ -5,25 +5,39 @@ import createHome from './components/home';
 import createMenu from './components/menu';
 import createAbout from './components/about';
 
-createHome();
-
 const main = (function() {
+  createHome();
+
   const contentContainer = document.getElementById('content');
   const homeBtn = document.getElementById('home-btn');
   const menuBtn = document.getElementById('menu-btn');
   const aboutBtn = document.getElementById('about-btn');
 
+  homeBtn.style = "background-color: salmon";
+
   homeBtn.addEventListener('click', (e) => {
+    e.target.style = "background-color: salmon";
+    menuBtn.style = "background-color: lightgray";
+    aboutBtn.style = "background-color: lightgray";
+
     contentContainer.innerText = "";
     createHome();
   })
 
   menuBtn.addEventListener('click', (e) => {
+    e.target.style = "background-color: salmon";
+    homeBtn.style = "background-color: lightgray";
+    aboutBtn.style = "background-color: lightgray";
+
     contentContainer.innerHTML = "";
     createMenu();
   })
 
   aboutBtn.addEventListener('click', (e) => {
+    e.target.style = "background-color: salmon";
+    homeBtn.style = "background-color: lightgray";
+    menuBtn.style = "background-color: lightgray";
+
     contentContainer.innerHTML = "";
     createAbout();
   })
